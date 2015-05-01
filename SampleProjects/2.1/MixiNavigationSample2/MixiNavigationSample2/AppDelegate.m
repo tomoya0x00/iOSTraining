e58933dc7ea1f4c952ad7d07f58945bf3e535887
tomoya.miwa <tomoyann@gmail.com>
2015-05-01 13:25:00 +0900

    done modify2.1

--- SampleProjects/2.1/MixiNavigationSample2/MixiNavigationSample2/AppDelegate.m
+++ SampleProjects/2.1/MixiNavigationSample2/MixiNavigationSample2/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [[UIBarButtonItem appearance] setTintColor:[UIColor blackColor]];
     return YES;
 }
 
